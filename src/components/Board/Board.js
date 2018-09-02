1fd7776c78f034fcc6c21b72cc764d3c81bd3705
Ramtin Khalatbari <VERSAYANA@outlook.com>
2018-09-02 19:21:27 +0430

    Added display and pressed effect

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -4,30 +4,36 @@ import Pad from '../Pad/Pad';
 import pads from '../../pads';
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: '',
+    };
+    this.playAudio = this.playAudio.bind(this);
+  }
+  playAudio(id, display) {
+    document.getElementById(id).play();
+    this.setState({
+      display: display,
+    });
+  }
   render() {
     return (
       <div id="drum-machine">
+        <div id="panel">
+          <span id="display">{this.state.display}</span>
+        </div>
         <div id="pad">
-          {pads.map((pad) => (
+          {pads.map((pad, index) => (
             <Pad
+              key={index}
               audio={pad.audio}
               keyText={pad.key}
               code={pad.code}
               display={pad.display}
+              playAudio={this.playAudio}
             />
           ))}
-          {/* <Pad
-            audio="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
-            k="Q"
-          />
-          <Pad k="W" />
-          <Pad k="E" />
-          <Pad k="A" />
-          <Pad k="S" />
-          <Pad k="D" />
-          <Pad k="Z" />
-          <Pad k="X" />
-          <Pad k="C" /> */}
         </div>
       </div>
     );
--- src/components/Pad/Pad.js
+++ src/components/Pad/Pad.js
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Pad.css';
 
-const Pad = ({ keyText, audio, display, code }) => {
-  const playAudio = () => {
-    document.getElementById(keyText).play();
-  };
-  return (
-    <div className="drum-pad" onClick={playAudio}>
-      {keyText}
-      <audio id={keyText}>
-        <source src={audio} />
-      </audio>
-    </div>
-  );
-};
+class Pad extends Component {
+  constructor(props) {
+    super(props);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.playAudio = this.playAudio.bind(this);
+    this.state = {
+      pressed: false,
+    };
+  }
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyPress);
+  }
+  // componentDidUpdate(nextProps, nextState) {
+  //   if (nextState.pressed === true) {
+  //     this.setState({
+  //       pressed: false,
+  //     });
+  //   }
+  // }
+  handleKeyPress(e) {
+    if (this.props.code === e.keyCode) {
+      this.playAudio();
+    }
+  }
+  playAudio() {
+    this.setState({
+      pressed: true,
+    });
+    setTimeout(() => {
+      this.setState({
+        pressed: false,
+      });
+    }, 100);
+    this.props.playAudio(this.props.keyText, this.props.display);
+  }
+  render() {
+    return (
+      <div
+        className={this.state.pressed ? 'drum-pad pressed' : 'drum-pad '}
+        onClick={this.playAudio}
+      >
+        {this.props.keyText}
+        <audio id={this.props.keyText}>
+          <source src={this.props.audio} />
+        </audio>
+      </div>
+    );
+  }
+}
 
 export default Pad;
